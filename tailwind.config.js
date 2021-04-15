b074fdec5c91e260c011016769f23df50a633af7
Odumz <odumz20@gmail.com>
2021-04-15 23:43:14 +0100

    app initialization

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./public/**/*.html', './public/**/*.js', './src/**/*.vue'],
+  prefix: 'tw-',
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    screens: {},
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
